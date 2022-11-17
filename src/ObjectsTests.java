ce30abd71fb69eff20228453f9996966ad36c085
RubanovDmitri <rubanovdp@gmail.com>
2022-11-17 19:57:10 +0200

    +Class work17.11.22

---
+++ src/ObjectsTests.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ObjectsTests {
+
+	@Test
+	void wrapperTest() {
+		Integer a = 11;
+		Integer b = 11;
+		assertEquals(a, b);
+		Integer c = 201;
+		Integer d = 201;
+		assertEquals(c, d);
+		assertTrue(a == b);
+		assertFalse(c == d);
+		assertTrue(c.equals(d));
+	}
+
+	@Test
+	void stringTest() {
+		String hello = "hello";
+		char[] helloAr = hello.toCharArray();
+		assertEquals('h', helloAr[0]);
+		System.out.println((byte) helloAr[2]);
+	}
+}
---
+++ src/Strings.java
@@ -0,0 +1,15 @@
+
+public class Strings {
+	/**
+	 * 
+	 * @param str1
+	 * @param str2
+	 * @return true if str2 is an anagram of str1 O(n)
+	 * 1. the same length
+	 * 2. the same symbols just in different order
+	 */
+public static boolean isAnagram(String str1, String str2) {
+	boolean res = false;
+	return res;
+}
+}
