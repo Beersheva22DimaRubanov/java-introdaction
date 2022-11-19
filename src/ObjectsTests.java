4532e38e60c22d9e237261e80bc107f77b5935c5
RubanovDmitri <rubanovdp@gmail.com>
2022-11-19 15:35:38 +0200

    add sortStrind method

--- src/ObjectsTests.java
+++ src/ObjectsTests.java
@@ -25,12 +25,33 @@ public class ObjectsTests {
 		System.out.println((byte) helloAr[2]);
 
 	}
-	
+
 	@Test
-	void isAnagramTest(){
+	void isAnagramTest() {
 		assertTrue(Strings.isAnagram("hello world", "world hello"));
 		assertFalse(Strings.isAnagram("hello world", "world"));
 		assertTrue(Strings.isAnagram(" ", " "));
 		assertTrue(Strings.isAnagram("thing", "night"));
+		String word = "yellow";
+		assertTrue(Strings.isAnagram(word, "loweyl"));
+		assertTrue(Strings.isAnagram(word, "elolyw"));
+		assertTrue(Strings.isAnagram(word, "wolley"));
+		assertTrue(Strings.isAnagram(word, "loleyw"));
+		assertFalse(Strings.isAnagram(word, ""));
+		assertFalse(Strings.isAnagram(word, "yellob"));
+		assertFalse(Strings.isAnagram(word, "yello"));
+		assertFalse(Strings.isAnagram(word, "yelllo"));
+	}
+
+	@Test
+	void sortStringNumbersTest() {
+		String[] arr1 = { "-128", "2", "-4", "54", "128", "-76", "123" };
+		String[] sortedArr1 = { "-128", "-76", "-4", "2", "54", "123", "128" };
+		String[] arr2 = { "5", "-37", "15", "2", "22", "120", "118", "-110", "5", "2", "1", "0", "127", "-128", "1", "5" };
+		String[] sortedArr2 = { "-128", "-110", "-37", "0", "1", "1", "2", "2", "5", "5", "5", "15", "22", "118", "120", "127" };
+		Strings.sortStringNumbers(arr2);
+		Strings.sortStringNumbers(arr1);
+		assertArrayEquals(sortedArr1, arr1);
+		assertArrayEquals(sortedArr2, arr2);
 	}
 }
--- src/Strings.java
+++ src/Strings.java
@@ -9,18 +9,42 @@ public class Strings {
 	 */
 	public static boolean isAnagram(String str1, String str2) {
 		boolean res = false;
+		int count = 0;
 		char[] str1Arr = str1.toCharArray();
 		char[] str2Arr = str2.toCharArray();
 		byte[] helper = new byte[128];
-		if(str1Arr.length == str2Arr.length) {
-			for(int i = 0; i<str1Arr.length; i++) {
-				helper[str1Arr[i]] ++;
-				helper[str2Arr[i]] --;
+		if (str1Arr.length == str2Arr.length) {
+			for (int i = 0; i < str1Arr.length; i++) {
+				helper[str1Arr[i]]++;
+				helper[str2Arr[i]]--;
 			}
-			for(int i = 0; i<helper.length; i++) {
-				res = helper[i] == 0;
+			for (int i = 0; i < helper.length; i++) {
+				if (helper[i] != 0)
+					count++;
 			}
+			if (count == 0)
+				res = true;
 		}
 		return res;
+	}
+
+	public static void sortStringNumbers(String[] array) {
+		int[] helper = new int[257];
+		for (int i = 0; i < array.length; i++) {
+			Integer num = Integer.parseInt(array[i]);
+			helper[num + 128] += 1;
+		}
+		int n = 0;
+		for (Integer i = 0; i < helper.length; i++) {
+			if (helper[i] != 0) {
+				while(helper[i] != 0) {
+					Integer num = i - 128;
+					array[n] = num.toString();
+					n++;
+					helper[i] --;
+				}
+				
+			}
+		}
 	}
 }
