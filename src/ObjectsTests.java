9c9ad83f4eca3d31c92ae2eee49c40bdb454d863
RubanovDmitri <rubanovdp@gmail.com>
2022-11-23 20:10:25 +0200

    +add regex for octet and ipv4 methods

--- src/ObjectsTests.java
+++ src/ObjectsTests.java
@@ -22,16 +22,15 @@ public class ObjectsTests {
 		String hello = "hello";
 		char[] helloAr = hello.toCharArray();
 		assertEquals('h', helloAr[0]);
-		System.out.println((byte) helloAr[2]);
 
 	}
 
 	@Test
 	void isAnagramTest() {
-		assertTrue(Strings.isAnagram("hello world", "world hello"));
-		assertFalse(Strings.isAnagram("hello world", "world"));
-		assertTrue(Strings.isAnagram(" ", " "));
-		assertTrue(Strings.isAnagram("thing", "night"));
+//		assertTrue(Strings.isAnagram("hello world", "world hello"));
+//		assertFalse(Strings.isAnagram("hello world", "world"));
+//		assertTrue(Strings.isAnagram(" ", " "));
+//		assertTrue(Strings.isAnagram("thing", "night"));
 		String word = "yellow";
 		assertTrue(Strings.isAnagram(word, "loweyl"));
 		assertTrue(Strings.isAnagram(word, "elolyw"));
@@ -53,5 +52,37 @@ public class ObjectsTests {
 		Strings.sortStringNumbers(arr1);
 		assertArrayEquals(sortedArr1, arr1);
 		assertArrayEquals(sortedArr2, arr2);
+	}
+	
+	@Test
+	void javaVariableTest() {
+		assertTrue("java".matches(Strings.javaNameExp()));
+		assertFalse("1java".matches(Strings.javaNameExp()));
+		assertFalse("_".matches(Strings.javaNameExp()));
+		assertTrue("__".matches(Strings.javaNameExp()));
+		assertTrue("java_1_2".matches(Strings.javaNameExp()));
+		assertTrue("$".matches(Strings.javaNameExp()));
+		assertFalse("$ _".matches(Strings.javaNameExp()));
+	}
+	
+	@Test
+	void ipV40ctetTest() {
+		assertTrue("123".matches(Strings.ipV40ctet()));
+		assertTrue("0".matches(Strings.ipV40ctet()));
+		assertTrue("255".matches(Strings.ipV40ctet()));
+		assertFalse("256".matches(Strings.ipV40ctet()));
+		assertTrue("089".matches(Strings.ipV40ctet()));
+		assertTrue("009".matches(Strings.ipV40ctet()));
+	}
+	
+	@Test
+	void ipV4Test() {
+//		assertTrue("089.045.120.230".matches(Strings.ipV4()));
+		assertTrue("0.0.0.0".matches(Strings.ipV4()));
+		assertTrue("123.245.213.224".matches(Strings.ipV4()));
+		assertTrue("255.255.255.255".matches(Strings.ipV4()));
+		assertFalse("123.345.1234.123".matches(Strings.ipV4()));
+		assertFalse("123.".matches(Strings.ipV4()));
+		assertFalse("_/".matches(Strings.ipV4()));
 	}
 }
--- src/Strings.java
+++ src/Strings.java
@@ -9,41 +9,78 @@ public class Strings {
 	 */
 	public static boolean isAnagram(String str1, String str2) {
 		boolean res = false;
-		int count = 0;
-		char[] str1Arr = str1.toCharArray();
-		char[] str2Arr = str2.toCharArray();
-		byte[] helper = new byte[128];
-		if (str1Arr.length == str2Arr.length) {
-			for (int i = 0; i < str1Arr.length; i++) {
-				helper[str1Arr[i]]++;
-				helper[str2Arr[i]]--;
+		if (str1.length() == str2.length()) {
+			res = true;
+			byte[] helper = new byte[Byte.MAX_VALUE];
+			fillHelper(helper, str1);
+			int index = 0;
+			int length = str1.length();
+			while (index < length && res) {
+				int indSymb = (int) str2.charAt(index);
+				if (helper[indSymb] == 0) {
+					res = false;
+				} else {
+					index++;
+					helper[indSymb]--;
+				}
 			}
-			for (int i = 0; i < helper.length; i++) {
-				if (helper[i] != 0)
-					count++;
-			}
-			if (count == 0)
-				res = true;
 		}
 		return res;
 	}
 
+	private static void fillHelper(byte[] helper, String str1) {
+		int length = str1.length();
+		for (int i = 0; i < length; i++) {
+			int ind = str1.charAt(i);
+			helper[ind]++;
+		}
+	}
+
 	public static void sortStringNumbers(String[] array) {
-		int[] helper = new int[257];
+		int offset = Byte.MAX_VALUE + 1;
+		int[] helper = new int[offset * 2];
+		fillHelper(helper, array, offset);
+		sortFromHelper(array, helper, offset);
+
+	}
+
+	private static void sortFromHelper(String[] array, int[] helper, int offset) {
+		int arrayInd = 0;
+		for (int i = 0; i < helper.length; i++) {
+			for (int j = 0; j < helper[i]; j++) {
+				array[arrayInd++] = getStringNumberFromIndex(i, offset);
+			}
+		}
+
+	}
+
+	private static String getStringNumberFromIndex(int index, int offset) {
+		int number = index - offset;
+		return Integer.toString(number);
+	}
+
+	private static void fillHelper(int[] helper, String[] array, int offset) {
 		for (int i = 0; i < array.length; i++) {
-			Integer num = Integer.parseInt(array[i]);
-			helper[num + 128] += 1;
+			int index = getIndexFromStringNumber(array[i], offset);
+			helper[index]++;
 		}
-		int i = 0;
-		int j = 0;
-		while (i < helper.length) {
-			if (helper[i] != 0) {
-				Integer num = i - 128;
-				array[j] = num.toString();
-				j++;
-				helper[i]--;
-			} else
-				i++;
-		}
+	}
+
+	private static int getIndexFromStringNumber(String string, int offset) {
+		int number = Integer.parseInt(string);
+		return number + offset;
+	}
+	
+	public static String javaNameExp() {
+		
+		return "[a-zA-Z$][\\w$]*|_[\\w$]+";
+	}
+	
+	public static String ipV40ctet() {
+		return "(2([0-4][0-9]|5[0-5])|[0-1]\\d\\d|[0-9]{1,2})";
+	}
+	
+	public static String ipV4() {
+		return "((2([0-4][0-9]|5[0-5])|[0-1]\\d\\d|[0-9]{1,2})\\.){3}(2([0-4][0-9]|5[0-5])|[0-1]\\d\\d|[0-9]{1,2})";
 	}
 }
